3341c3ebb88eb022b13587ec5a94b203c90ccecf
Adam Harpur <adam@harpur.io>
2019-05-11 15:15:30 -0500

    feat: added boards and notes service

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BoardsController } from './boards/boards.controller';
 import { NotesController } from './notes/notes.controller';
+import { BoardsService } from './boards/boards.service';
+import { NotesService } from './notes/notes.service';
 
 @Module({
   imports: [],
   controllers: [AppController, BoardsController, NotesController],
-  providers: [AppService],
+  providers: [AppService, BoardsService, NotesService],
 })
 export class AppModule {}
