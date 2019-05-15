fccc6a310fb129e40cc3afc2192401ab78e01aaa
Adam Harpur <adam@harpur.io>
2019-05-14 19:45:10 -0500

    feat: created notes module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,15 +1,13 @@
-import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { NotesController } from './notes/notes.controller';
-import { NotesService } from './notes/notes.service';
-
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardsModule } from './boards/boards.module';
+import { NotesModule } from './notes/notes.module';
 @Module({
-  imports: [TypeOrmModule.forRoot(), BoardsModule],
-  controllers: [AppController, NotesController],
-  providers: [AppService, NotesService],
+  imports: [TypeOrmModule.forRoot(), BoardsModule, NotesModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
