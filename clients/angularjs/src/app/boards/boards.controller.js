731f1dd9d5dea78214e6e0be521de5829b656e96
Adam Harpur <adam@harpur.io>
2019-05-20 13:45:33 -0500

    feat: setup boards model

--- clients/angularjs/src/app/boards/boards.controller.js
+++ clients/angularjs/src/app/boards/boards.controller.js
@@ -1,22 +1,8 @@
 'use strict';
 
-angular.module('noteorious', [])
-    .controller("BoardsCtrl", function ($scope) {
+angular.module('noteorious', ['common'])
+    .controller("BoardsCtrl", function ($scope, BoardsModel) {
         var ctrl = $scope;
-        ctrl.boards = [{
-            id: '1',
-            title: 'Board One',
-            description: 'This is board one',
-            isActive: 1,
-            createdAt: new Date('2019-05-15T02:41:39.111Z'),
-            updatedAt: new Date('2019-05-15T02:41:39.111Z'),
-        }, {
-            id: '2',
-            title: 'Board 2',
-            description: 'This is board two',
-            isActive: 1,
-            createdAt: new Date('2019-05-15T02:41:39.111Z'),
-            updatedAt: new Date('2019-05-15T02:41:39.111Z'),
-        }]
+        ctrl.boards = BoardsModel.all()
 
     });
--- clients/angularjs/src/app/common/models/boards.model.js
+++ clients/angularjs/src/app/common/models/boards.model.js
@@ -3,4 +3,12 @@
 angular.module('common')
     .service('BoardsModel', function ($http) {
         var service = this;
+
+        function extract(result) {
+            return result.data;
+        }
+        service.all = function () {
+            console.log('BOARDS::all')
+        };
+
     });
