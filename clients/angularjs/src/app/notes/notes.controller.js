f6a4b3d526946fd9eb1db2b102fc31b05f2047a9
Adam Harpur <adam@harpur.io>
2019-05-20 14:21:42 -0500

    feat: update to notes controller

--- clients/angularjs/src/app/notes/notes.controller.js
+++ clients/angularjs/src/app/notes/notes.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('noteorious', ['common'])
-    .controller("BoardsCtrl", function ($scope, NotesModel) {
+    .controller("NotesCtrl", function ($scope, NotesModel) {
         var ctrl = $scope;
         NotesModel.all().then(data => ctrl.boards = data)
     });
