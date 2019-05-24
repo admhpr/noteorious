46b30b48002cd55f66263715571850b385804869
Adam Harpur <adam@harpur.io>
2019-05-24 08:07:03 -0500

    feat: hooked up router service

--- clients/angularjs/src/app/core/index.js
+++ clients/angularjs/src/app/core/index.js
@@ -1,11 +1,12 @@
 const angular = require('angular');
+import routerSetup from './services/routes/router.service'
 import BoardsModel from './models/boards.model';
 import {
     ENDPOINT_URI
 } from './config/const';
 
-const core = angular.module('app.core', [])
+
+export default angular.module('app.core', [])
+    .constant(routerSetup)
     .constant('ENDPOINT_URI', ENDPOINT_URI)
     .service('BoardsModel', BoardsModel)
-
-export default core
