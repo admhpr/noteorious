53bc88659ed5bc1cb4e86c5282605f7ff6bdc3fb
Adam Harpur <adam@harpur.io>
2019-06-04 11:59:11 -0500

    feat: added app directive

--- clients/angularjs/src/index.js
+++ clients/angularjs/src/index.js
@@ -1,8 +1,12 @@
-const angular = require('angular');
+import angular from 'angular';
 import '../../assets/style.css';
+import {
+    appDirective
+} from './app.directive';
 import components from './app/components';
 import uiRouter from 'angular-ui-router';
 import core from './app/core'
 
 
-export default angular.module('app', [uiRouter, core.name, components.name]);
+angular.module('app', [uiRouter, core.name, components.name])
+    .directive('app', appDirective)
