3c74fdee7cf43c83434272f337d0a236694c40f5
Adam Harpur <adam@harpur.io>
2019-06-08 08:22:04 -0500

    feat: added some logic for boards model

--- clients/angularjs/src/app/core/models/boards.model.js
+++ clients/angularjs/src/app/core/models/boards.model.js
@@ -1,53 +1,54 @@
 const boardsModel = ($http, ENDPOINT_URI, $q) => {
-    let allBoards = [];
-    let id = 0;
+  let allBoards = [];
+  let id = 0;
 
-    const getUrl = () => {
-        return ENDPOINT_URI + '/boards';
-    };
+  const getUrl = () => {
+    return ENDPOINT_URI + "/boards";
+  };
 
-    const extract = ({
-        data
-    }) => {
-        allBoards = data
-    };
+  const extract = ({ data }) => {
+    allBoards = data;
+  };
 
-    const handleError = (e) => {
-        console.error(e)
-        return [{
-            title: "server down",
-            description: "server down"
-        }]
+  const handleError = e => {
+    console.error(e);
+    return [
+      {
+        title: "server down",
+        description: "server down"
+      }
+    ];
+  };
+
+  const get = () => {
+    console.log(`get::BoardsModel`);
+    return $http
+      .get(getUrl())
+      .then(extract)
+      .catch(handleError);
+  };
+
+  const getOne = query => {
+    const board = allBoards.find(board => board.id === query.id);
+    if (board) {
+      return $q.when(board);
     }
+  };
 
-    const get = () => {
-        console.log(`get::BoardsModel`)
-        return $http.get(getUrl())
-            .then(extract).catch(handleError);
-    };
+  const getState = () => {
+    return allBoards;
+  };
 
-    const getOne = (query) => {
+  const create = data => {};
 
-    };
-
-    const getState = () => {
-        return allBoards;
-    };
-
-    const create = (data) => {
-
-    };
-
-    return {
-        get,
-        getOne,
-        getState,
-        create
-    };
+  return {
+    get,
+    getOne,
+    getState,
+    create
+  };
 };
 
-boardsModel.$inject = ['$http', 'ENDPOINT_URI', '$q'];
+boardsModel.$inject = ["$http", "ENDPOINT_URI", "$q"];
 
-export {
-    boardsModel
-};
+export { boardsModel };
