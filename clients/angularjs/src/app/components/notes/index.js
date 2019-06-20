b0c24b860550d0c9d9fca257211c91b0a806d9f7
Adam Harpur <adam@harpur.io>
2019-06-20 10:57:54 -0500

    feat: added notes index

--- clients/angularjs/src/app/components/notes/index.js
+++ clients/angularjs/src/app/components/notes/index.js
@@ -1,1 +1,8 @@
 //TODO: add index for notes
+import angular from "angular";
+import { notesComponent } from "./notes.component";
+import core from "../../core";
+
+export default angular
+  .module("notes", [core.name])
+  .component("notes", notesComponent);
