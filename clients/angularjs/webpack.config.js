d5616e0086a20db73f5237e9ae0e84b0293c0b5c
Adam Harpur <adam@harpur.io>
2019-06-27 01:17:46 -0500

    chore: added aliases

--- clients/angularjs/webpack.config.js
+++ clients/angularjs/webpack.config.js
@@ -1,46 +1,50 @@
 const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const {
+  CleanWebpackPlugin
+} = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-  mode: "development",
-  context: __dirname + "/src",
-  entry: {
-    app: "./index.js",
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: "./index.html",
-    }),
-  ],
-  output: {
-    filename: "[name].[hash].js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
+    mode: "development",
+    context: __dirname + "/src",
+    entry: {
+      app: "./index.js",
+    },
+    resolve: {
+      alias: {
+        "@components": path.resolve(__dirname, 'src/app/components/'),
+        "@core": path.resolve(__dirname, 'src/app/core/')
       },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
-            options: {
-              minimize: true,
+      plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+          template: "./index.html",
+        }),
+      ],
+      output: {
+        filename: "[name].[hash].js",
+        path: path.resolve(__dirname, "dist"),
+      },
+      module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader",
             },
+          },
+          {
+            test: /\.html$/,
+            use: [{
+              loader: "html-loader",
+              options: {
+                minimize: true,
+              },
+            }, ],
+          },
+          {
+            test: /\.css$/,
+            use: ["style-loader", "css-loader"],
           },
         ],
       },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
-};
+    };
