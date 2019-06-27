4396d0da203fa47ef36e0fecfc926b672d0f2e66
Adam Harpur <adam@harpur.io>
2019-06-27 01:29:46 -0500

    chore: added aliases for jest and webpack

--- clients/angularjs/jest.config.js
+++ clients/angularjs/jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   setupFilesAfterEnv: ["./jest.setup.js"],
+  moduleNameMapper: {
+    '^@components/(.*)$': '<rootDir>/src/app/components/$1',
+    '^@core/(.*)$': '<rootDir>/src/app/core/$1',
+  },
 };
--- clients/angularjs/webpack.config.js
+++ clients/angularjs/webpack.config.js
@@ -4,47 +4,49 @@ const {
 } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-    mode: "development",
-    context: __dirname + "/src",
-    entry: {
-      app: "./index.js",
+  mode: "development",
+  context: __dirname + "/src",
+  entry: {
+    app: "./index.js",
+  },
+  resolve: {
+    alias: {
+      "@assets": path.resolve(__dirname, '../assets/'),
+      "@components": path.resolve(__dirname, 'src/app/components/'),
+      "@core": path.resolve(__dirname, 'src/app/core/')
     },
-    resolve: {
-      alias: {
-        "@components": path.resolve(__dirname, 'src/app/components/'),
-        "@core": path.resolve(__dirname, 'src/app/core/')
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
+  output: {
+    filename: "[name].[hash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
       },
-      plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-          template: "./index.html",
-        }),
-      ],
-      output: {
-        filename: "[name].[hash].js",
-        path: path.resolve(__dirname, "dist"),
+      {
+        test: /\.html$/,
+        use: [{
+          loader: "html-loader",
+          options: {
+            minimize: true,
+          },
+        }, ],
       },
-      module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-              loader: "babel-loader",
-            },
-          },
-          {
-            test: /\.html$/,
-            use: [{
-              loader: "html-loader",
-              options: {
-                minimize: true,
-              },
-            }, ],
-          },
-          {
-            test: /\.css$/,
-            use: ["style-loader", "css-loader"],
-          },
-        ],
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
       },
-    };
+    ],
+  },
+}
