be3da4b9dada0533f8d22bd447d8d1e5cab67ba9
Adam Harpur <adam@harpur.io>
2019-07-01 01:22:38 -0500

    test: boards controller

--- clients/angularjs/src/app/components/boards/boards.spec.js
+++ clients/angularjs/src/app/components/boards/boards.spec.js
@@ -1,1 +1,18 @@
-describe("BoardsController", () => {});
+import angular from "angular";
+import boards from "@components/boards/index";
+import * as angularMock from "angular-mocks";
+import { BoardsController } from "./boards.controller";
+describe("BoardsController", () => {
+  beforeEach(angular.mock.module(boards.name));
+  let $rootScope;
+  let makeController;
+
+  beforeEach(window.module(boards.name));
+  // eslint-disable-next-line no-undef
+  beforeEach(inject(_$rootScope_ => {
+    $rootScope = _$rootScope_;
+    makeController = injectables => {
+      return new BoardsController(injectables);
+    };
+  }));
+});
--- clients/angularjs/src/app/core/models/test/notes.spec.js
+++ clients/angularjs/src/app/core/models/test/notes.spec.js
@@ -1,23 +1,21 @@
 import angular from "angular";
 import core from "@core/index";
 import * as angularMock from "angular-mocks";
-import {
-  NotesModel
-} from "../notes.model";
+import { NotesModel } from "../notes.model";
 
-describe("Notes Model", function () {
+describe("Notes Model", function() {
   beforeEach(angular.mock.module(core.name));
 
   var notesModel;
   // eslint-disable-next-line no-undef
   beforeEach(inject(NotesModel => (notesModel = NotesModel)));
 
-  it("run the test", function () {
+  it("run the test", function() {
     expect(1 + 1).toEqual(2);
   });
 
-  describe("get", function () {
-    it("should set the state to an array of objects", function () {
+  describe("get", function() {
+    it("should set the state to an array of objects", function() {
       notesModel.get().then(console.log(notesModel.getState()));
       expect(notesModel.getState().length).toBeGreaterThan(1);
     });
