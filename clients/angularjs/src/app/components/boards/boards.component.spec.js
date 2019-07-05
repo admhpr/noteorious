be8f09f8f01ca15942d6b531b308e4f1b8156155
Adam Harpur <adam@harpur.io>
2019-07-05 01:44:09 -0500

    test: boards component

--- clients/angularjs/src/app/components/boards/boards.component.spec.js
+++ clients/angularjs/src/app/components/boards/boards.component.spec.js
@@ -1,11 +1,21 @@
 import angular from "angular";
 import boards from "@components/boards/index";
 import * as angularMock from "angular-mocks";
+import { boardsComponent } from "./boards.component";
+import template from "./boards.tmpl.html";
+import { BoardsModel } from "@core/models/boards.model";
 import { BoardsController } from "./boards.controller";
+
+jest.mock("@core/models/boards.model");
+
 describe("BoardsController", () => {
   beforeEach(angular.mock.module(boards.name));
   let $rootScope;
   let makeController;
+
+  beforeAll(() => {
+    console.log(BoardsModel);
+  });
 
   beforeEach(window.module(boards.name));
   // eslint-disable-next-line no-undef
@@ -19,6 +29,36 @@ describe("BoardsController", () => {
   describe("module", () => {
     it("should have an appropriate name", () => {
       expect(boards.name).toBe("boards");
+    });
+  });
+
+  describe("component", () => {
+    let ddo;
+    beforeEach(() => {
+      ddo = boardsComponent;
+    });
+
+    it("should have the right template", () => {
+      expect(ddo.template).toEqual(template);
+    });
+
+    it("should have the right controller", () => {
+      expect(ddo.controller).toEqual(BoardsController);
+    });
+
+    it("should have an isolate scope", () => {
+      expect(ddo.scope).toEqual(expect.any(Object));
+    });
+
+    it("should use controllerAs", () => {
+      expect(ddo.controllerAs).toEqual(expect.any(String));
+    });
+  });
+
+  describe("controller", () => {
+    it("should have boards", () => {
+      const controller = makeController();
+      expect(controller.boards).any(Array);
     });
   });
 });
