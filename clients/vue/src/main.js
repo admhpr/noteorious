c8669822074ad05badb55ece880ba3738f00a505
Adam Harpur <adam@harpur.io>
2019-07-21 07:35:07 -0500

    feat: router setup

--- clients/vue/src/main.js
+++ clients/vue/src/main.js
@@ -1,12 +1,9 @@
 import Vue from "vue";
-import App from "./App.vue";
 import router from "./router";
-
+import App from "./App";
 Vue.config.productionTip = false;
 
 new Vue({
-  el: "#app",
   router,
-  template: "<App/>",
-  components: { App }
-});
+  render: h => h(App)
+}).$mount("#app");
