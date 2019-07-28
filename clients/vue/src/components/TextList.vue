d8b630f55873b31273d0c1feb2ac1440a4eb0b7c
Adam Harpur <adam@harpur.io>
2019-07-28 06:14:25 -0500

    feat: text list implementation

---
+++ clients/vue/src/components/TextList.vue
@@ -0,0 +1,24 @@
+<template>
+  <List :items="listItems">
+    <div slot-scope="{item}" class="list-item">{{item.text}}</div>
+  </List>
+</template>
+
+<script>
+import List from "./base/List.vue";
+export default {
+  name: "app",
+  components: {
+    List
+  },
+  data() {
+    return {
+      listItems: [
+        { text: "First item", icon: "fa fa-user" },
+        { text: "Second item", icon: "fa fa-copy" },
+        { text: "Third item", icon: "fa fa-cut" }
+      ]
+    };
+  }
+};
+</script>
