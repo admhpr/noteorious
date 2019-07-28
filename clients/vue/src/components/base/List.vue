88dcec41a70a81ad2e58bde51a581dd7393347cd
Adam Harpur <adam@harpur.io>
2019-07-28 06:09:13 -0500

    feat: base list component

---
+++ clients/vue/src/components/base/List.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <slot v-for="item in items" :item="item"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      default: () => []
+    }
+  }
+};
+</script>
